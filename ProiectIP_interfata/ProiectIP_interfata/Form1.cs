39bfb26ba44a728e8c9d40c655d92bc0f5194e72
Colipca Teodor <teodor-costin.colipca@student.tuiasi.ro>
2021-05-26 22:45:19 +0300

    ?????

--- ProiectIP_interfata/ProiectIP_interfata/Form1.cs
+++ ProiectIP_interfata/ProiectIP_interfata/Form1.cs
@@ -7,13 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using LogIn;
+using MySql.Data.MySqlClient;
 namespace ProiectIP_interfata
 {
     public partial class Form1 : Form
     {
+        private MySqlConnection _conn;
+        private UsersManager _usersManager;
         public Form1()
         {
+            string MyConStr = "Server=localhost;Database=proiect_ip;uid=teodor;pwd=Dismas67!";
+
+            _conn = new MySqlConnection(MyConStr);
+            _usersManager = new UsersManager(_conn);
             InitializeComponent();
         }
 
@@ -34,8 +41,18 @@ namespace ProiectIP_interfata
 
         private void button_login_Click(object sender, EventArgs e)
         {
-            DestinationPickControl flightPicker = new DestinationPickControl();
-            showControl(flightPicker);
+            string mail = textBox_login_email.Text;
+            string parola = textBox_login_password.Text;
+            int id = _usersManager.Logare(mail, parola);
+            if(id==-1)
+            {
+                MessageBox.Show("Date invalide");
+            }
+            else {
+                DestinationPickControl flightPicker = new DestinationPickControl();
+                showControl(flightPicker);
+            }
+
         }
 
         public void showControl(Control control)
@@ -48,6 +65,23 @@ namespace ProiectIP_interfata
 
             Content.Controls.Add(control);
 
+        }
+
+        private void button_signup_Click(object sender, EventArgs e)
+        {
+            string nume = textBox_signup_name.Text;
+            string prenume = textBox_signup_surname.Text;
+            string mail = textBox_signup_email.Text;
+            string parola = textBox_signup_password.Text;
+            string verificare_parola = textBox_signup_repeat_password.Text;
+            if(_usersManager.signUp(nume,prenume,mail,parola,verificare_parola))
+            {
+                MessageBox.Show("V-ati inregistrat cu succes!");
+            }
+            else
+            {
+                MessageBox.Show("Parolele nu corespund!");
+            }
         }
     }
 }
