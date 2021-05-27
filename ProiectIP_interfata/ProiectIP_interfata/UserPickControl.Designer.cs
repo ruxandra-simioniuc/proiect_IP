a728f7db9de222a407f3758996e6e64055dafff6
ruxandra-simioniuc <ruxandra-simioniuc@student.tuiasi.ro>
2021-05-27 13:26:57 +0300

    refacut UserPick + singleton + pass crypting

--- ProiectIP_interfata/ProiectIP_interfata/UserPickControl.Designer.cs
+++ ProiectIP_interfata/ProiectIP_interfata/UserPickControl.Designer.cs
@@ -46,12 +46,14 @@
             this.label9 = new System.Windows.Forms.Label();
             this.button_login = new System.Windows.Forms.Button();
             this.button_signup = new System.Windows.Forms.Button();
+            this.UserPickContent = new System.Windows.Forms.Panel();
+            this.UserPickContent.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox_login_email
             // 
             this.textBox_login_email.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_login_email.Location = new System.Drawing.Point(121, 173);
+            this.textBox_login_email.Location = new System.Drawing.Point(105, 163);
             this.textBox_login_email.Name = "textBox_login_email";
             this.textBox_login_email.Size = new System.Drawing.Size(236, 35);
             this.textBox_login_email.TabIndex = 0;
@@ -59,7 +61,7 @@
             // textBox_login_password
             // 
             this.textBox_login_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_login_password.Location = new System.Drawing.Point(121, 254);
+            this.textBox_login_password.Location = new System.Drawing.Point(105, 244);
             this.textBox_login_password.Name = "textBox_login_password";
             this.textBox_login_password.PasswordChar = '*';
             this.textBox_login_password.Size = new System.Drawing.Size(236, 35);
@@ -111,7 +113,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(109, 73);
+            this.label1.Location = new System.Drawing.Point(93, 63);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(192, 69);
             this.label1.TabIndex = 7;
@@ -131,7 +133,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(30, 179);
+            this.label3.Location = new System.Drawing.Point(14, 169);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(74, 29);
             this.label3.TabIndex = 9;
@@ -141,7 +143,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(21, 260);
+            this.label4.Location = new System.Drawing.Point(5, 250);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(83, 29);
             this.label4.TabIndex = 10;
@@ -151,7 +153,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(556, 179);
+            this.label5.Location = new System.Drawing.Point(540, 169);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(78, 29);
             this.label5.TabIndex = 11;
@@ -161,7 +163,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(523, 260);
+            this.label6.Location = new System.Drawing.Point(507, 250);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(111, 29);
             this.label6.TabIndex = 12;
@@ -171,7 +173,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(560, 336);
+            this.label7.Location = new System.Drawing.Point(544, 326);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(74, 29);
             this.label7.TabIndex = 13;
@@ -181,7 +183,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(551, 417);
+            this.label8.Location = new System.Drawing.Point(535, 407);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(83, 29);
             this.label8.TabIndex = 14;
@@ -191,7 +193,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(469, 488);
+            this.label9.Location = new System.Drawing.Point(453, 478);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(165, 29);
             this.label9.TabIndex = 15;
@@ -201,12 +203,13 @@
             // 
             this.button_login.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_login.Location = new System.Drawing.Point(121, 345);
+            this.button_login.Location = new System.Drawing.Point(105, 335);
             this.button_login.Name = "button_login";
             this.button_login.Size = new System.Drawing.Size(236, 44);
             this.button_login.TabIndex = 16;
             this.button_login.Text = "Log in";
             this.button_login.UseVisualStyleBackColor = false;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
             // 
             // button_signup
             // 
@@ -218,31 +221,42 @@
             this.button_signup.TabIndex = 17;
             this.button_signup.Text = "Sign up";
             this.button_signup.UseVisualStyleBackColor = false;
+            this.button_signup.Click += new System.EventHandler(this.button_signup_Click);
+            // 
+            // UserPickContent
+            // 
+            this.UserPickContent.Controls.Add(this.button_login);
+            this.UserPickContent.Controls.Add(this.label9);
+            this.UserPickContent.Controls.Add(this.label8);
+            this.UserPickContent.Controls.Add(this.label7);
+            this.UserPickContent.Controls.Add(this.label6);
+            this.UserPickContent.Controls.Add(this.label5);
+            this.UserPickContent.Controls.Add(this.label4);
+            this.UserPickContent.Controls.Add(this.label3);
+            this.UserPickContent.Controls.Add(this.label1);
+            this.UserPickContent.Controls.Add(this.textBox_login_password);
+            this.UserPickContent.Controls.Add(this.textBox_login_email);
+            this.UserPickContent.Location = new System.Drawing.Point(12, 3);
+            this.UserPickContent.Name = "UserPickContent";
+            this.UserPickContent.Size = new System.Drawing.Size(1360, 900);
+            this.UserPickContent.TabIndex = 18;
             // 
             // UserPickControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.button_signup);
-            this.Controls.Add(this.button_login);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox_signup_passwordRepeat);
             this.Controls.Add(this.textBox_signup_password);
             this.Controls.Add(this.textBox_signup_email);
             this.Controls.Add(this.textBox_signup_name);
             this.Controls.Add(this.textBox_signup_surname);
-            this.Controls.Add(this.textBox_login_password);
-            this.Controls.Add(this.textBox_login_email);
+            this.Controls.Add(this.UserPickContent);
             this.Name = "UserPickControl";
             this.Size = new System.Drawing.Size(1360, 900);
+            this.UserPickContent.ResumeLayout(false);
+            this.UserPickContent.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -268,5 +282,6 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Button button_login;
         private System.Windows.Forms.Button button_signup;
+        private System.Windows.Forms.Panel UserPickContent;
     }
 }
